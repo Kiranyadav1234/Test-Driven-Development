670a5a28ca0edc3ef7e14bc0d48e09e63fc54db8
Kiranyadav1234 <Kiran_Yadav@mckinsey.com>
2022-02-13 21:54:16 +0530

    feat :add print armstrong number till given range

---
+++ armstrongNumberGenerator.js
@@ -0,0 +1,35 @@
+function findArmstrongNumber(range)
+{
+    if( typeof(range) === 'string')
+    {
+       throw  new Error("Invalid Input");
+    }
+    for(let index=1;index<=range;index++)
+    {
+        console.log(isArmstrongNumber(index));
+        if(isArmstrongNumber(index))
+           console.log(index);
+
+    }
+       
+}  
+function isArmstrongNumber(number)
+{
+    if( typeof(number) === 'string')
+    {
+       throw  new Error("Invalid Input");
+    }
+    let temp = number;
+    let sum = 0;
+    while(temp>0)
+    {
+
+        let remainder = temp%10;
+        sum +=remainder*remainder*remainder;
+        temp =parseInt(temp/10);
+
+    }
+    return (sum === number);  
+}
+
+module.exports={isArmstrongNumber,findArmstrongNumber};
